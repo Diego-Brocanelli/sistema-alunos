896fd46c5f8863d6ca968bfeb1a37ab7b5c525e7
Diego Brocanelli <diegod2@msn.com>
2015-04-18 11:41:30 -0300

    Criação da view para pesquisa de alunos e estrutura base de conteúdo.

---
+++ resources/views/alunos/select.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title', 'Home - Sistema de gerenciamento de alunos')
+
+@section('content')
+    <div class='row'>
+        <div class='col-sm-12'>
+            <div class="page-header">
+                <h1>{{$viewTitle}}</h1>
+            </div>
+        </div>
+    </div>
+@stop
