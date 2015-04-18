ce63d395064f548356aa694ac96ce5e88d501bf3
Diego Brocanelli <diegod2@msn.com>
2015-04-18 11:30:52 -0300

    Estruturação da view home do sistema, contendo os textos de saudação e botões para acesso rápido aos recursos disponíveis no sistema.

--- resources/views/alunos/index.blade.php
+++ resources/views/alunos/index.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.master')
 
-@section('title', 'Page Title')
+@section('title', 'Home - Sistema de gerenciamento de alunos')
 
 @section('content')
-    <p>This is my body content.</p>
+    <div class='row'>
+        <div class='col-sm-12'>
+            <div class="page-header">
+                <h1>{{$viewTitle}}</h1>
+                <h3>{{$viewSubtitle}}</h3>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 jumbotron">
+            <h4>{{$actionsContainer}}</h4>
+            <a class='{{$btnClass}}' href="{{$linkPesquisar}}">{{$labelPequisar}}</a>
+        </div>
+    </div>
 @stop
