deb3e291bc535dcea590df8add912e9495cc0bd8
Diego Brocanelli <diegod2@msn.com>
2015-04-18 11:29:17 -0300

    Inclusão de chamada da view alunos.index, e inclusão de textos a serem exibidos na view (utilisando blade).

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,6 +13,13 @@ class UserController extends Controller
 {
     public function index()
     {
-        return view('alunos.index');
+        return view('alunos.index', [
+            'viewTitle'        => 'Bem vindo ao sistema de gerenciamento de alunos.',
+            'viewSubtitle'     => 'Gerencie com facilidade e agilidade todos os alunos.',
+            'actionsContainer' => 'Ações disponíveis no sistema.',
+            'btnClass'         => 'btn btn-primary btn-lg',
+            'linkPesquisar'    => '/alunos/pesquisar',
+            'labelPequisar'    => 'Pesquisar Alunos',
+        ]);
     }
 }
