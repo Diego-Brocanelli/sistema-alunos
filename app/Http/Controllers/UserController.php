49b06ce8c0cededb914fab9edba0fbe2f53aaff6
Diego Brocanelli <diegod2@msn.com>
2015-04-18 13:32:41 -0300

    Implementação de query básica ao banco de dados na tabela alunos, para retorno de dados e exibição do grid (Futuramente será implementado com Doctrine)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,25 +10,49 @@ use App\Http\Controllers\Controller;
 class UserController extends Controller
 {
     /**
-     * REsponsável pela view home do sistema
+     * Responsável pela view home do sistema
      * @return View
      */
     public function index()
     {
-        return view('alunos.index', [
+        $viewContent = [
             'viewTitle'        => 'Bem vindo ao sistema de gerenciamento de alunos.',
             'viewSubtitle'     => 'Gerencie com facilidade e agilidade todos os alunos.',
             'actionsContainer' => 'Ações disponíveis no sistema.',
             'btnClass'         => 'btn btn-primary btn-lg',
             'linkPesquisar'    => '/alunos/pesquisar',
             'labelPequisar'    => 'Pesquisar Alunos',
-        ]);
+        ];
+        return view('alunos.index', $viewContent);
     }
     
+    /**
+     * Responsável pela view de pesquisa de alunos
+     * @return View
+     */
     public function select()
     {
-        return view('alunos.select',[
+        $viewContent = [
             'viewTitle' => 'Pesquisa de alunos',
-        ]);
+            'backToHome' => '/',
+        ];
+        $db = new \Illuminate\Support\Facades\DB();
+        $alunos = $db::select(""
+                . "select "
+                . "id, "
+                . "nome, "
+                . "sobrenome, "
+                . "DATE_FORMAT(data_nascimento,'%m/%d/%Y') as data_nascimento, "
+                . "idade, "
+                . "serie, "
+                . "clase, "
+                . "data_cadastro, "
+                . "hora_cadastro "
+                . "from "
+                . "alunos;");
+        
+        $viewContent['alunos'] = $alunos;
+
+        return view('alunos.select',$viewContent);
     }
 }
