a1dc97ba2eb6ed3957903f27272ee0cf94a3d474
Diego Brocanelli <diegod2@msn.com>
2015-04-18 10:59:59 -0300

    Criação de layout default para o sistema.

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="/css/bootstrap/css/bootstrap.min.css">
+        <title>App Name - @yield('title')</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+        <script src="/js/jquery-1.10.2.min.js"></script>
+        <script src="/css/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
